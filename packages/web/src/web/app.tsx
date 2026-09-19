95a5e7e755d8e472c5871fb18b7b65f07d86e302
orshengnudor <www.rightpart1h@gmail.com>
2026-09-19 18:52:39 +0100

    remove third-party analytics beacon from build

--- packages/web/src/web/app.tsx
+++ packages/web/src/web/app.tsx
@@ -4,7 +4,6 @@ import Leaderboard from "./pages/leaderboard";
 import Recap from "./pages/recap";
 import Milestone from "./pages/milestone";
 import { Provider } from "./components/provider";
-import { AgentFeedback } from "@runablehq/website-runtime";
 
 function App() {
   return (
@@ -16,8 +15,6 @@ function App() {
         <Route path="/milestone" component={Milestone} />
         <Route component={Index} />
       </Switch>
-      {/* Do not remove, off by default, activated by parent iframe via postMessage */}
-      {import.meta.env.DEV && <AgentFeedback />}
     </Provider>
   );
 }
